bfea0440af028a90fd2c17c9dce23180b5284b9c
Caio R <114326411+Caio2909@users.noreply.github.com>
2024-07-08 23:47:59 -0300

    Add files via upload

---
+++ Ruralbooks.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.uffrj.ruralbooks;
+
+/**
+ *
+ * @author lofts
+ */
+public class Ruralbooks {
+
+    public static void main(String[] args) {
+       java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new painelPrincipal().setVisible(true);
+            }
+        });
+}
+}
