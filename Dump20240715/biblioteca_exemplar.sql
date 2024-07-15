0c29603fe6e4ebd4a66ac0a387bfc8d5554713bc
Caio R <114326411+Caio2909@users.noreply.github.com>
2024-07-15 19:05:46 -0300

    Add files via upload

---
+++ Dump20240715/biblioteca_exemplar.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: biblioteca
+-- ------------------------------------------------------
+-- Server version	8.0.38
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `exemplar`
+--
+
+DROP TABLE IF EXISTS `exemplar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exemplar` (
+  `isbn` varchar(13) NOT NULL,
+  `livro_id` int NOT NULL,
+  PRIMARY KEY (`isbn`),
+  KEY `livro_id` (`livro_id`),
+  CONSTRAINT `exemplar_ibfk_1` FOREIGN KEY (`livro_id`) REFERENCES `livros` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exemplar`
+--
+
+LOCK TABLES `exemplar` WRITE;
+/*!40000 ALTER TABLE `exemplar` DISABLE KEYS */;
+INSERT INTO `exemplar` VALUES ('ISBN1-1',1),('ISBN1-2',1),('ISBN2-1',2),('ISBN2-2',2),('ISBN3-1',3),('ISBN3-2',3),('ISBN4-1',4),('ISBN4-2',4),('ISBN5-1',5),('ISBN5-2',5),('ISBN6-1',6),('ISBN6-2',6),('ISBN7-1',7),('ISBN7-2',7),('ISBN8-1',8),('ISBN8-2',8),('ISBN9-1',9),('ISBN9-2',9),('ISBN10-1',10),('ISBN10-2',10),('ISBN11-1',11),('ISBN11-2',11),('ISBN12-1',12),('ISBN12-2',12);
+/*!40000 ALTER TABLE `exemplar` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-15 19:04:24
